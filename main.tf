ad70b07c11906162e644ce05c50164f6abc2d5d4
Florian Dambrine <florian@gumgum.com>
2018-09-24 00:36:52 -0700

    Init repo

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 ### Backend definition
 
-terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
+# terraform {
+#   # The configuration for this backend will be filled in by Terragrunt
+#   backend "s3" {}
+# }
+
+provider "aws" {
+  region = "${var.aws_region}"
 }
 
 ### Module Main
